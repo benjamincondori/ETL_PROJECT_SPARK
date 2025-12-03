138fce36b1aa09da003706f5a1c6f6ca0794358b
Benjamin Condori Vasquez <benjamincondori7@gmail.com>
2025-12-03 15:03:41 -0400

    Modifique el spark_sesion.py

--- core/spark_session.py
+++ core/spark_session.py
@@ -3,14 +3,8 @@ from pyspark.sql import SparkSession
 from .config import JDBC_DRIVER_PATH
 
 def create_spark_session():
-    # Establecer variables de entorno necesarias para la JVM
-    # Esto es crucial en Linux, como ya lo experimentaste.
-    # Asume que JAVA_HOME y SPARK_HOME están en el PATH o definidos globalmente
-
-    # Para ser extremadamente seguros, si se ejecuta con Python directamente:
-    # os.environ['PYSPARK_PYTHON'] = ... (ruta a tu .venv)
-    # os.environ['PYSPARK_DRIVER_PYTHON'] = ... (ruta a tu .venv)
-    
+    """Crea y retorna una SparkSession configurada para el proyecto ETL."""
+        
     spark = (
         SparkSession.builder
         .appName("ETL_QoS_Automation")
@@ -18,8 +12,8 @@ def create_spark_session():
         
         # Configuración de estabilidad y JDBC
         .config("spark.sql.execution.arrow.pyspark.enabled", "false")
-        .config("spark.jars", JDBC_DRIVER_PATH)
-        .config("spark.driver.extraClassPath", JDBC_DRIVER_PATH)
+        # .config("spark.jars", JDBC_DRIVER_PATH)
+        # .config("spark.driver.extraClassPath", JDBC_DRIVER_PATH)
         
         .getOrCreate()
     )
